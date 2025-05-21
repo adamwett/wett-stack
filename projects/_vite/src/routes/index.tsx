1a68cf4ffb2d8cb983ac34f0db5591882e13a5e3
adamwett <adamwettreich@gmail.com>
2025-05-21 13:02:39 -0400

    update vite template

--- projects/_vite/src/routes/index.tsx
+++ projects/_vite/src/routes/index.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@repo/ui/components/button";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: HomeComponent,
-})
+export const Route = createFileRoute("/")({
+    component: HomeComponent,
+});
 
 function HomeComponent() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+    return (
+        <div className="p-2">
+            <h3>Welcome Home!</h3>
+            <Button onClick={() => alert("hello!")}>
+                <p>Hello World!</p>
+            </Button>
+        </div>
+    );
 }
