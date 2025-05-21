932d8690fdfa3f82131033a291b3fcbb13988916
adamwett <adamwettreich@gmail.com>
2025-05-21 14:18:04 -0400

    copy vite template

---
+++ projects/agent-smith-frontend/src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@repo/ui/components/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@repo/ui/components/card';
+import { createFileRoute } from '@tanstack/react-router';
+export const Route = createFileRoute('/')({
+  component: HomeComponent,
+});
+
+function HomeComponent() {
+  return (
+    <div className='p-2'>
+      <h3>Welcome Home!</h3>
+      <Button onClick={() => alert('hello!')} variant='default'>
+        Hello, world!
+      </Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      <div className='h-32 w-32 bg-red-500' />
+    </div>
+  );
+}
